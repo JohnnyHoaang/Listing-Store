db77768620456247b2e9c80dc8dc2f0b13d61c84
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-04-12 10:01:24 -0400

    added website administration application

--- web_admin_app/apps.py
+++ web_admin_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WebAdministrationConfig(AppConfig):
+class WebAdminAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'web_administration'
+    name = 'web_admin_app'
