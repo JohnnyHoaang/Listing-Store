dbe329874c099a3f8b03739afd38181f78519574
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-04-19 13:37:42 -0400

    Added my files back

--- product_listing_app/models.py
+++ product_listing_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=2000)
+    post_description = models.CharField(max_length=2000)
+    post_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post_title} {self.post_description} ({self.post_date})'
--- product_listing_app/views.py
+++ product_listing_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    post = Post.objects.order_by('-title')[:5]
+    template = loader.get_template('product_listing_app/product_posts.html')
+    context = {
+        'prod_posts': post,
+    }
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'product_posts.html')
+'''
+def create_post(request):
+    prod_posts = Posts.objects.get(prod_posts=prod_posts)
+    template = loader.get_template('product_listing_app/product_posts.html')
+    context = {
+        'prod_posts': prod_posts,
+    }
+
+    return HttpResponse(template.render(context, request))
+'''
