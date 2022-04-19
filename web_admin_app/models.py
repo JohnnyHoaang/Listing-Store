6f752706b9b9f5d5e4b0284bd8141bd3d218efd6
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-04-18 22:02:19 -0400

    added relation with Account

--- web_admin_app/models.py
+++ web_admin_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from ..user_management_app.models import Account
 # Create your models here.
 class Group(models.Model):
     group_name = models.CharField(max_length=100)
-
+    account = models.ManyToManyField(Account, through='PersonGroup', through_fields=('account'))
     def __str__(self):
         return self.group_name
+class PersonGroup(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.account}'
