e7d38d899a5665a0160004daaf04a142ee8d2c8c
Dan Willis <dannjoumene@gmail.com>
2022-04-19 12:06:41 -0400

    fixed view for messaging_app

--- messaging_app/product_listing_project/urls.py
+++ messaging_app/product_listing_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_management_app.urls'), name='index'),
+    path('', include('messaging.app.urls'))
 ]
