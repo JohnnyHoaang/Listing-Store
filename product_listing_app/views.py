a8597a904079bb68ba8c97963737ed19115fb4a5
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-04-21 10:17:00 -0400

    changing html page to home page

--- product_listing_app/views.py
+++ product_listing_app/views.py
@@ -5,13 +5,13 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    post = Post.objects.order_by('-title')[:5]
-    template = loader.get_template('product_listing_app/product_posts.html')
-    context = {
-        'prod_posts': post,
-    }
-    return HttpResponse(template.render(context, request))
-    #return render(request, 'product_posts.html')
+    #post = Post.objects.order_by('-title')[:5]
+    #template = loader.get_template('product_listing_app/product_posts.html')
+    #context = {
+        #'prod_posts': post,
+    #}
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'product_posts.html')
 '''
 def create_post(request):
     prod_posts = Posts.objects.get(prod_posts=prod_posts)
