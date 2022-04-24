a82fef1393386b5982032ad99a8eee7c63708ace
Maria Maria <mariamaria.ramirez0@gmail.com>
2022-04-24 17:05:19 -0400

    restarted messaging system and started t move towards a dm functionallity rather than a forum chat functionallity

--- messaging_app/models.py
+++ messaging_app/models.py
@@ -1,9 +1,21 @@
+import datetime
+from django.utils import timezone
+from distutils.command.upload import upload
 from django.db import models
-from datetime import datetime
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User=get_user_model()
+
+# Create your models here:
+class ThreadModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = '+')
+    reciever = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+
 class Message(models.Model):
-    value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
-    user = models.CharField(max_length=1000000)
-    value = models.CharField(max_length=1000000)
+    thread = models.ForeignKey('ThreadModel', related_name='+', on_delete=models.CASCADE, blank=True, null=True)
+    sender_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+', null=True)
+    reciever_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+', null=True)
+    body = models.CharField(max_length=1000, null=True)
+    image = models.ImageField(upload_to='uploads/message_photos', blank=True, null=True)
+    date = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
