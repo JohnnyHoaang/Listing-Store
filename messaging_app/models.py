b3b30236d5ae1f52ed5242c9e1a86213d6e2b956
Maria Maria <mariamaria.ramirez0@gmail.com>
2022-04-24 17:42:00 -0400

    fixed typo

--- messaging_app/models.py
+++ messaging_app/models.py
@@ -2,7 +2,8 @@ import datetime
 from django.utils import timezone
 from distutils.command.upload import upload
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here:
 class ThreadModel(models.Model):
