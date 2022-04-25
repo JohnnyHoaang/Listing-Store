6adb6e52837d9f68cc93be88dd69704cfd8c6898
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-04-25 09:29:50 -0400

    Added css styling to static folder and changed html to get styling to work. Added to the Post model to include more of the specific fields plus the html structure itself

--- product_listing_app/models.py
+++ product_listing_app/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     post_title = models.CharField(max_length=2000)
+    #maybe a dropdown here?
+    post_category = models.CharField(max_length=2000, default="Books")
+    # post_price = 
+    # post_keywords = 
     post_description = models.CharField(max_length=2000)
+    #maybe a drop down for this?
+    post_status = models.CharField(max_length=2000, default="status")
+    #post_image = 
     post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.post_title} {self.post_description} ({self.post_date})'
+        post_value = f'Title: {self.post_title} \n Category: {self.post_category}'
+        return f'{post_value}'
