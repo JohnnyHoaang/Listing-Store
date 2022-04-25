9177ae2efd2d05d98e38629bac2af88acb0f019d
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-04-25 11:10:03 -0400

    added image and keyword list fields

--- product_listing_app/models.py
+++ product_listing_app/models.py
@@ -27,10 +27,10 @@ class Post(models.Model):
     post_title = models.CharField(max_length=2000)
     post_category = models.CharField(max_length=2000, choices=categories_for_posts, default=books)
     post_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
-    # post_keywords = 
+    post_keywords = models.TextField(blank=True, null=True)
     post_description = models.TextField(blank=True, null=True)
     post_status = models.CharField(max_length=2000, choices=status_for_posts, default=pg_13)
-    # post_image = models.ImageField()
+    post_image = models.ImageField(upload_to='', blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
