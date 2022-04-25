52f3139d1f8062b409b840d689aabf5aab586234
Johnny Hoang <johnny.hoang@dawsoncollege.qc.ca>
2022-04-25 10:16:49 -0400

    Assigned required permissions to groups and created a dashboard for admin

--- web_admin_app/static/script.js
+++ web_admin_app/static/script.js
@@ -25,11 +25,52 @@ function load(){
 }
 
 function createTable(data){
+    createHeaders()
     console.log(data)
     data.forEach(e=> {
-        let p = document.createElement('p');
-        p.textContent = e.username + " " + e.groups__name
-        global.div.appendChild(p)
+
+        // let p = document.createElement('p');
+        // p.textContent = e.username + " " + e.groups__name
+        // global.div.appendChild(p)
+        let tr = document.createElement('tr')
+        let username = document.createElement('td')
+        username.textContent = e.username
+        let group = document.createElement('td')
+        group.textContent = e.groups__name
+        tr.appendChild(username)
+        tr.appendChild(group)
+        global.table.append(tr)
+        createButtons(tr)
     })
     
+}
+
+function createHeaders(){
+    let tr = document.createElement('tr')
+    let username = document.createElement('th')
+    username.textContent="Username"
+    let group = document.createElement('th')
+    group.textContent="Group"
+    tr.append(username, group)
+    //tr.appendChild(group)
+    global.table.append(tr)
+}
+
+function createButtons(tr){
+    let edit = document.createElement('button')
+    edit.textContent = "Edit Details"
+    edit.id = "edit"
+    let modify = document.createElement('button')
+    modify.textContent = "Modify Groups"
+    modify.id = "modify"
+    let flag = document.createElement('button')
+    flag.textContent = "Flag"
+    flag.id = "flag"
+    let warn = document.createElement('button')
+    warn.textContent = "Warn"
+    warn.id = "warn"
+    let delete_but = document.createElement('button')
+    delete_but.textContent = "Delete"
+    delete_but.id = "delete"
+    tr.append(edit, modify, flag, warn, delete_but)
 }
