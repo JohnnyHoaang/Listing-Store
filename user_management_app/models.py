72d0fc2b31c3c23dbc5417a36aa0bae387caec60
Dan Willis <dannjoumene@gmail.com>
2022-04-30 17:28:37 -0400

    Still working on getting avatar pic working

--- user_management_app/models.py
+++ user_management_app/models.py
@@ -1,8 +1,12 @@
+from distutils.command.upload import upload
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    avatar = models.BinaryField(editable=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.BinaryField(editable=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- user_management_app/views.py
+++ user_management_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from user_management_app.models import Profile
+from .models import Profile
 from .forms import CustomUserCreationForm
 from django.contrib.auth import logout
 
@@ -18,10 +18,11 @@ def signup(request):
             new_user.first_name = form.cleaned_data['first_name']
             new_user.last_name = form.cleaned_data['last_name']
             new_user.save()
-            from PIL import Image
-            avatar = Image.open(r'static/default_avatar.png')
-            profile = Profile(user=new_user, avatar=avatar)
-            profile.save()
+            # import cv2
+            # img = cv2.imread(r'static/default_avatar.jpg', 2)
+            # binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+            # profile = Profile(user=new_user, avatar=binary_img)
+            # profile.save()
             return redirect('login')
     else:  
         form = CustomUserCreationForm()  
