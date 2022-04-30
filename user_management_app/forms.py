dbe01e0caff24f8e350ab08595c454d55857c71c
Dan Willis <dannjoumene@gmail.com>
2022-04-29 23:18:25 -0400

    Added some styling to form

--- user_management_app/forms.py
+++ user_management_app/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 # This is a custom form from which I do my own validation
 class CustomUserCreationForm(UserCreationForm):  
-    username = forms.CharField(label='username', min_length=4, max_length=150)  
-    first_name = forms.CharField(label='first_name', min_length=2, max_length=150) 
-    last_name = forms.CharField(label='last_name', min_length=2, max_length=150)     
-    email = forms.EmailField(label='email')  
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput)  
+    username = forms.CharField(label='Username', min_length=4, max_length=150)  
+    first_name = forms.CharField(label='First Name', min_length=2, max_length=150) 
+    last_name = forms.CharField(label='Last Name', min_length=2, max_length=150)     
+    email = forms.EmailField(label='Email')  
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)  
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)  
     
     class Meta:
