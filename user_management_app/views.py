55337d2f24503cbf2513df93f780d68ee698f7c1
Dan Willis <dannjoumene@gmail.com>
2022-04-30 22:26:17 -0400

    Made the nav-bar dynamic + added avatar pic + changed styling of the website

--- user_management_app/views.py
+++ user_management_app/views.py
@@ -2,27 +2,36 @@ from django.shortcuts import render, redirect
 from .models import Profile
 from .forms import CustomUserCreationForm
 from django.contrib.auth import logout
+from django.http import HttpResponse
+from django.template import loader
+import base64
 
 # Create your views here.
 def index(request):
-    return render(request, "base.html")
+    encoded_image = None
+    if request.user.is_authenticated:
+        if request.user.profile.avatar != None:
+            encoded_image = base64.b64encode(request.user.profile.avatar).decode("utf-8")
+    context = {
+        'encoded_image': encoded_image
+    }
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render(context, request))
 
 
 def signup(request):
     form = None
     if request.method == 'POST':  
         form = CustomUserCreationForm(request.POST)  
-        print(f"valid: {form.is_valid()}")
         if form.is_valid(): 
             new_user = form.save(commit = False)
             new_user.first_name = form.cleaned_data['first_name']
             new_user.last_name = form.cleaned_data['last_name']
             new_user.save()
-            # import cv2
-            # img = cv2.imread(r'static/default_avatar.jpg', 2)
-            # binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-            # profile = Profile(user=new_user, avatar=binary_img)
-            # profile.save()
+            with open('user_management_app/static/default_avatar.png', 'rb') as imagefile:
+                bytestring = imagefile.read()
+                profile = Profile(user=new_user, avatar=bytestring)
+                profile.save()
             return redirect('login')
     else:  
         form = CustomUserCreationForm()  
