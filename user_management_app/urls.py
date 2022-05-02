1c20b18f74788092a999c7c7d2cba282066f0b0a
Dan Willis <dannjoumene@gmail.com>
2022-05-02 19:16:29 -0400

    Added profile section where you can modify user info, change avatar and see services(messaging, adminstration)

--- user_management_app/urls.py
+++ user_management_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('signup/', views.signup, name='signup'),   
+    path('signup/', views.signup, name='signup'),  
+    path('profile/', views.profile, name='profile'), 
 ]
