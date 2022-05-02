29ddf9a575c6aeb09e215d8dc4493a14515697ae
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-05-02 10:37:07 -0400

    Changed dropdown menu options to have less unneccessary code

--- product_listing_app/models.py
+++ product_listing_app/models.py
@@ -3,26 +3,19 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     books = 'Books'
-    cd = 'CDs'
-    videos = 'Videos'
-    movies = 'Movies'
-    tv_show = 'TV Shows'
-    categories_for_posts = [
-        (books,'Books'),
-        (cd, 'CDs'),
-        (videos, 'Videos'),
-        (movies, 'Movies'),
-        (tv_show, 'TV Shows'),
-    ]
     pg_13 = 'PG13'
-    status_r = 'R'
-    pg = 'PG'
-    explicit = 'Explicit'
+    categories_for_posts = [
+        ('Books','Books'),
+        ('CDs','CDs'),
+        ('Videos','Videos'),
+        ('Movies','Movies'),
+        ('TV Shows','TV Shows'),
+    ]
     status_for_posts = [
-        (pg_13, 'PG13'),
-        (status_r, 'R'),
-        (pg, 'PG'),
-        (explicit, 'Explicit'),
+        ('PG13','PG13'),
+        ('R','R'),
+        ('PG','PG'),
+        ('Explicit', 'Explicit'),
     ]
     post_title = models.CharField(max_length=2000)
     post_category = models.CharField(max_length=2000, choices=categories_for_posts, default=books)
