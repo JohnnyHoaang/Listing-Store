cc35989e64891362163b972b83595db0075810e6
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-05-02 19:30:16 -0400

    changed variable names to be clearer

--- web_admin_app/urls.py
+++ web_admin_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('dashboard/admin_users',views.admin_manage_users, name='admin_users'),
     path('dashboard/admin_items', views.admin_manage_items, name='admin_items'),
     path('dashboard/admin', views.admin_access, name='admin'),
-    path('dashboard/admin_items/user/<str:username>' , views.delete_user_ai, name='delete_users_ai'),
-    path('dashboard/admin_users/user/<str:username>' , views.delete_user_au, name='delete_users_au'),
+    path('dashboard/admin_items/user/<str:username>' , views.delete_user_admin_items, name='delete_users_admin_items'),
+    path('dashboard/admin_users/user/<str:username>' , views.delete_user_admin_users, name='delete_users_admin_users'),
     path('dashboard/admin_items/post/<str:post_title>' , views.delete_post, name='delete_posts'),
 ]
