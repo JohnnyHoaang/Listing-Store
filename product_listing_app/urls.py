78f1fe420d16e7310f883f3f231595602bd0dc9e
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-05-02 09:25:01 -0400

    changing post listing content to home page

--- product_listing_app/urls.py
+++ product_listing_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.homepage, name='home'),
-    path('home/create', views.create_post, name='create_post'),
-    path('', views.DetailedPost.as_view(), name='detailed_post')
+    #path('home', views.homepage, name='home'),
+    #path('home/create', views.create_post, name='create_post'),
+    path ('',views.ListPosts.as_view(), name='posts'),
+    path('details', views.DetailedPost.as_view(), name='detailed_post')
 ]
