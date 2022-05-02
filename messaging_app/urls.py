fe18d3ab9c5376fbd8e7cbd5eabc6a26c75ef8c5
Maria Maria <mariamaria.ramirez0@gmail.com>
2022-05-02 09:37:50 -0400

    changed path in urls from ListThreads to createThreads to initiatie a new conversation

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -6,5 +6,5 @@ from .views import CreateThread, ListTreads
 urlpatterns = [
     path('messaging', views.home, name='message'),
     path('inbox/', ListTreads.as_view(), name='inbox'), 
-    path('inbox/create-thread', ListTreads.as_view(), name='create-thread')
+    path('inbox/create-thread', CreateThread.as_view(), name='create-thread')
 ]
