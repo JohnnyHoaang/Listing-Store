16a364e0d0032859ee5aa39cde2c08b6b73bbff0
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-05-08 15:52:57 -0400

    added debug and secret key to settings

--- product_listing_project/settings.py
+++ product_listing_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0y8e*jj@g$3=wp16cxs518t5ytno(5@dsa8zq5kaz2_y24jvr='
+SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = 'django-insecure-0y8e*jj@g$3=wp16cxs518t5ytno(5@dsa8zq5kaz2_y24jvr='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG'] == 'True'
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -139,3 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Redirect to home URL after logout
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+import django_heroku
+django_heroku.settings(locals())
