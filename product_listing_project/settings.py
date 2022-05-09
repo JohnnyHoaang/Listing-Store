bdd45d01a4b10d38e5c048dc4f48253e43a7de1c
Veronika P <veronika.pontolillo@dawsoncollege.qc.ca>
2022-05-09 09:17:57 -0400

    removed comment

--- product_listing_project/settings.py
+++ product_listing_project/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG'] == 'False'
-#DEBUG = True
 
 ALLOWED_HOSTS = []
 
