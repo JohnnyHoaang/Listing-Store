fdc5ecd23e2e6e66fb410bae79c39fee26a7004a
Maria Maria <mariamaria.ramirez0@gmail.com>
2022-05-09 04:00:16 -0400

    stil working on template redirection

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -5,8 +5,8 @@ from .views import CreateThread, ListThreads, ThreadView, CreateMessage
 
 urlpatterns = [
     path('', views.home, name='message'),
-    path('inbox/', ListThreads.as_view(), name='inbox'), 
-    path('inbox/create-thread/', CreateThread.as_view(), name='create-thread'),
-    path('inbox/<int:pk>', ThreadView.as_view(), name='thread'),
-    path('inbox/<int:pk>/create-message/', CreateMessage.as_view(), name='create-message')
+    path('inbox/', views.ListThreads.as_view(), name='inbox'), 
+    path('inbox/create-thread/', views.CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>', views.ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message/', views.CreateMessage.as_view(), name='create-message')
 ]
