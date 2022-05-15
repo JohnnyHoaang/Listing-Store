b2f2c89132f3b069f29eb829fca3ddff2b6d9cc4
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-05-14 21:12:17 -0400

    added login decorator for post creating

--- product_listing_app/urls.py
+++ product_listing_app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path ('posts/',PostView.as_view(), name="posts"),
-    path ('create/', CreatePost.as_view(), name='create'),
+    path ('create/', login_required(CreatePost.as_view(),login_url='/login'), name='create'),
     path ('details/<int:pk>', login_required(PostDetailView.as_view(), login_url='/login'), name='details'),
     path ('details/<int:pk>/edit/', EditPostView.as_view(), name='edit'),
     path ('details/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
