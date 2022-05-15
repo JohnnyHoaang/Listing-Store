c4b6e579943df443da68f724312960c89e8bcaf1
Maria Maria <mariamaria.ramirez0@gmail.com>
2022-05-15 17:59:29 -0400

    had to redo all the db, that is wht the migrations are all deleted

--- messaging_app/views.py
+++ messaging_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.shortcuts import render, redirect
 from django.template import loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
